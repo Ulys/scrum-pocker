b3b76035e460158369060cb09d735c44c33022a6
Olena Sovyn <olenasovyn@gmail.com>
2014-08-11 09:46:07 +0300

    CoffeCard creation started

---
+++ js/coffeecard.js
@@ -0,0 +1,30 @@
+/**
+ * @fileoverview file contain User Story module;
+ * @author olena_sovyn@epam.com
+ */
+
+/**
+ * @module CoffeeCard
+ */
+
+var CoffeeCard = ( function () {
+
+	"use strict";
+
+	//<div class="black_overlay"></div>
+	//<div class="coffee_content"><div></div><div></div></div>
+
+	function activateCoffeCard () {
+		document.createDocumentFragment();
+		document.body.appendChild();
+		document.body.appendChild();
+	}
+
+
+	function CoffeeCard () {
+		document.getElementsByClassName('coffee-card')[0].addEventListener('onclick', activateCoffeCard());
+	};
+
+
+	return CoffeeCard;
+})
